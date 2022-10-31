4277e0278f2e5b0972b3aa8b08b06ac64d882f4a
dika98 <114928701+stanger24@users.noreply.github.com>
2022-10-31 14:44:13 +0700

    Add files via upload

---
+++ perbandingan.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+main(){
+	int a,b;
+	
+	cout<<"masukkan variabel a : ";
+	cin>>a;
+	cout<<"masukkan variabel b : ";
+	cin>>b;
+	if (a>b){
+		cout<<"vareiabel a lebih besar dari pada variabel b"<<endl;
+	}
+	else if (a<b){
+		cout<<"variabel a lebih kecil dari pada variabel b"<<endl;
+		}	
+			
+	else if (a=b){
+			cout<<"variabel a sama besar dengan variabel b"<<endl;
+		}
+		
+		}
+	
